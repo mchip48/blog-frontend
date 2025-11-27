0e3ed0057811a8b03f2d374680c2933ffa217dee
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-26 20:55:54 -0600

    Messed more with styling and made everything fit better in the browser. Changed root font also

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,6 +23,7 @@ export function Header() {
         </div>
       </div>
     </nav>
+    
     </header>
   );
 }
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -2,17 +2,20 @@ export function PostsIndex( { postsProp, onShow } ) {
   return (
     <div id="posts-index">
       <h1>All {postsProp.length} Posts</h1>
-      {postsProp.map((post) => (
-        <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} />
-          <p>{post.body}</p>
-          <div>
-            
+      <div className="row row-cols-3 row-cols-md-4 g-4">
+        {postsProp.map((post) => (
+          <div key={post.id} className="col">
+            <div className="card h-100">
+              <img src={post.image} className="card-img-top object-fit-cover" style={{ height: "200px", width: "259px"}} alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text">{post.body}</p>
+              </div>
+              <button className="btn btn-primary" onClick={() => onShow(post)}>More Info</button>
+            </div>
           </div>
-          <button onClick={() => onShow(post)}>More Info</button>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
