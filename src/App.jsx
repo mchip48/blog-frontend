5eb3336c3c57911a0c8c6255971fd8ab11949f3b
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-08 20:11:02 -0600

    Created SignupPage.jsx and imported it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { PostsPage } from "./PostsPage";
 import axios from "axios";
+import { SignupPage } from "./SignupPage";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <SignupPage />
       <PostsPage />
       <Footer />
     </div>
