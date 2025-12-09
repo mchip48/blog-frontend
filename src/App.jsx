58532dbe23b5035daaa9586633e64e12bd64b4f0
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-08 20:58:44 -0600

    Created a LoginPage.jsx and updated App.jsx to correspond

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import { Header } from "./Header";
 import { PostsPage } from "./PostsPage";
 import axios from "axios";
 import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
 function App() {
+  const email = localStorage.getItem("email");
 
   return (
     <div className="container">
       <Header />
-      <SignupPage />
+      {!email && (
+        <>
+          <SignupPage />
+          <LoginPage />
+        </>
+      )}
       <PostsPage />
       <Footer />
     </div>
