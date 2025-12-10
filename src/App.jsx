7938ef89febef55b9545438053d643dee5d20632
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-09 19:14:16 -0600

    Installed react-router-dom and and set up routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,64 @@
-import { Footer } from "./Footer";
+import axios from "axios";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
 import { PostsPage } from "./PostsPage";
-import axios from "axios";
 import { SignupPage } from "./SignupPage";
 import { LoginPage } from "./LoginPage";
+import { Footer } from "./Footer";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
-function App() {
-  const email = localStorage.getItem("email");
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children : [
+      {
+        path: "/",
+        element: <PostsPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
 
-  return (
-    <div className="container">
-      <Header />
-      {!email && (
-        <>
-          <SignupPage />
-          <LoginPage />
-        </>
-      )}
-      <PostsPage />
-      <Footer />
-    </div>
-  )
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App
+
+
+
+
+
+// was in function App()
+
+
+// const email = localStorage.getItem("email");
+
+// <div className="container">
+//   <Header />
+//   {!email && (
+//     <>
+//       <SignupPage />
+//       <LoginPage />
+//     </>
+//   )}
+//   <PostsPage />
+//   <Footer />
+// </div>
